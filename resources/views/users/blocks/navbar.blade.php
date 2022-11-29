7fc3acc6ee50aef45688f4493d2611ce48da8c42
Reew52 <koydzxxx@gmail.com>
2022-11-29 08:05:24 +0700

    update view homepage

--- resources/views/users/blocks/navbar.blade.php
+++ resources/views/users/blocks/navbar.blade.php
@@ -91,7 +91,7 @@
                     </ul>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                    <a class="nav-link" href="{{route('admin.index')}}">Admin</a>
                   </li>
                 </ul>
               </div>
