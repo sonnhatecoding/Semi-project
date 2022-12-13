cf80e8a7c6e44e7400bb65851ea3437e4eaf6d63
Nam Son <sonnpnbh00204@gmail.com>
2022-12-13 13:58:55 +0700

    edit model final

--- app/Models/User/CartProductModel.php
+++ app/Models/User/CartProductModel.php
@@ -14,5 +14,5 @@ class CartProductModel extends Model
     [
         'color_id', 'brand_id','pro_ram','pro_oSystem','pro_iMemory','pro_warrantyPeriod','pro_image','pro_image1','pro_image2','pro_image3','pro_image4','pro_quatity','pro_price','pro_reducedPrice','pro_description','pro_origin','pro_name','pro_launchDate'
     ];
-    public $timestamps = false; 
+    public $timestamps = false; //timestampsa
 }
