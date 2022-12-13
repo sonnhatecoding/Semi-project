43e83ea8d6c7eaedf1bdd3e4d1706c5bcac1206e
Nam Son <sonnpnbh00204@gmail.com>
2022-12-13 13:57:11 +0700

    edit file cartproduct

--- app/Models/User/CartProductModel.php
+++ app/Models/User/CartProductModel.php
@@ -9,7 +9,7 @@ class CartProductModel extends Model
 {
     use HasFactory;
     public $table ="products"; //tro vao bang product
-    public $primaryKey = 'pro_id'; 
+    public $primaryKey = 'pro_id'; //tro vao pro_id
     public $fillable = 
     [
         'color_id', 'brand_id','pro_ram','pro_oSystem','pro_iMemory','pro_warrantyPeriod','pro_image','pro_image1','pro_image2','pro_image3','pro_image4','pro_quatity','pro_price','pro_reducedPrice','pro_description','pro_origin','pro_name','pro_launchDate'
